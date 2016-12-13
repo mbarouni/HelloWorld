f54fc992a34d8e6aded682a279eefe55b3a2f4af
Mohammad Barouni <barouni@bigstream.co>
2016-12-13 15:59:31 -0800

    Now it is merged

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,11 +1,7 @@
 public class HelloWorld1 {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-<<<<<<< HEAD
-      System.out.println("Hello, World");
-      System.out.println("second line coming from HelloWorld branch to test merger");
-=======
       System.out.println("Hello, World, this is the change from branch 1");
->>>>>>> master
+      System.out.println("second line coming from HelloWorld branch to test merger");
    }
 }
