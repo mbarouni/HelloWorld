b4d58d1ce17ab765ca9984b25f11f073e6940766
Mohammad Barouni <barouni@bigstream.co>
2016-12-14 11:27:07 -0800

    Test push example

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -3,5 +3,6 @@ public class HelloWorld1 {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World, this is the change from branch 1");
       System.out.println("second line coming from HelloWorld branch to test merger");
+      System.out.println("Let's hope that jenkins will pick the push trigger after this change");
    }
 }
