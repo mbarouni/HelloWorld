e576d72597d3fecf924aa3ff43fc32b0c60376c0
Mohammad Barouni <barouni@bigstream.co>
2016-12-14 11:43:26 -0800

    sdafa

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -3,6 +3,5 @@ public class HelloWorld1 {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World, this is the change from branch 1");
       System.out.println("second line coming from HelloWorld branch to test merger");
-      System.out.println("Let's hope that jenkins will pick the push trigger after this change");
-   }
+      System.out.println("Should be build 5"   }
 }
