7d58efb9136f7a265c9d9d7543147b93c73e728a
Mohammad Barouni <barouni@bigstream.co>
2016-12-14 16:40:19 -0800

    asdf

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -2,9 +2,7 @@ public class HelloWorld1 {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello World");
-      System.out.println("Let's see if jenkins is going to compile this and show it");
-let's just break the build
-break it even more
+      System.out.println("fixed the compile error");
    }
 
 }
