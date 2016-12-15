270e975ab2c60aab41b57926d6e4e0e40f6192bc
Mohammad Barouni <barouni@bigstream.co>
2016-12-14 18:07:12 -0800

    asdf

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -3,7 +3,7 @@ public class HelloWorld1 {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello World");
       System.out.println("fixed the compile error");
-	test
+	still broken
    }
 
 }
