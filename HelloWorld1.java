467cffdcaadbfc6c121f1531b1b43416893d977d
Mohammad Barouni <barouni@bigstream.co>
2016-12-15 17:31:46 -0800

    test

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -2,7 +2,7 @@ public class HelloWorld1 {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello World");
-      System.out.println("fixed the compile error");
+      System.out.println("fixed the compile error, i need success :)");
    }
 
 }
