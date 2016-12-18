a68acab0cf66f927e854581fa1c45f0b722414ef
Mohammad Barouni <barouni@bigstream.co>
2016-12-18 13:59:04 -0800

    just a change

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class HelloWorld1 {
    public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
       System.out.println("New file") // removed ; on purpose ;)
       System.out.println("Hello World");
       System.out.println("fixed the compile error, i need success :)");
