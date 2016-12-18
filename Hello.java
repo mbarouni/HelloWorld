2a03ef92420a572e3ef5deb225b3d422996ec5cc
Mohammad Barouni <barouni@bigstream.co>
2016-12-18 14:04:56 -0800

    asdfa

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class HelloWorld1 {
+public class Hello {
    public static void main(String[] args) {
       System.out.println("New file") // removed ; on purpose ;)
       System.out.println("Hello World");
