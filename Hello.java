a2f1d89d515932539abca0d1a79decf8b85c3334
Mohammad Barouni <barouni@bigstream.co>
2016-12-19 12:17:19 -0800

    Fixed the problem

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
-      System.out.println("New file") // removed ; on purpose ;)
+      System.out.println("New file"); // removed ; on purpose ;)
       System.out.println("Hello World");
       System.out.println("fixed the compile error, i need success :)");
 
