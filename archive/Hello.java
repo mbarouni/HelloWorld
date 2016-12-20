b50b6f4b2fb3ba99072796d3e334593fd9f70c74
Mohammad Barouni <barouni@bigstream.co>
2016-12-19 19:25:01 -0800

    fixed junit stuff

---
+++ archive/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+   public static void main(String[] args) {
+      String str = (new MessageGenerator()).getWelcomeMessage();
+      System.out.println(str);
+//      System.out.println("Hello World");
+//      System.out.println("fixed the compile error, i need success :)");
+
+   }
+
+}
